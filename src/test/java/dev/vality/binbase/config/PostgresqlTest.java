c50373162c08d3e362891260f83350da68249927
Egor Cherniak <cherniak3@yandex.ru>
2025-06-09 14:57:15 +0300

    Fix test

--- src/test/java/dev/vality/binbase/config/PostgresqlTest.java
+++ src/test/java/dev/vality/binbase/config/PostgresqlTest.java
@@ -1,9 +1,6 @@
 package dev.vality.binbase.config;
 
 import dev.vality.testcontainers.annotations.postgresql.PostgresqlTestcontainerSingleton;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,8 +9,6 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@PostgresqlTestcontainerSingleton
+@PostgresqlTestcontainerSingleton(truncateTables = false)
 public @interface PostgresqlTest {
 }
